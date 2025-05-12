52aa394ee9ba2aa870281686a80c0074b3bc47e3
rajesha <rajeshan@gmail.com>
2025-05-12 09:28:00 +0530

    added css

--- src/components/CreateProduct.js
+++ src/components/CreateProduct.js
@@ -43,77 +43,162 @@ const CreateProduct = () => {
         }
     }
 
+    // return (
+    //     <div>
+    //         <h1>Create Product</h1>
+
+    //         {serverErrors?.errors && (
+    //             <>
+    //                 <p>These errors are prohibited from creating the category:</p>
+    //                 <ul>
+    //                     {serverErrors.errors.map((ele, i) => (
+    //                         <li key={i}>{ele.msg}</li>
+    //                     ))}
+    //                 </ul>
+    //             </>
+    //         )}
+    //         <form onSubmit={handleSubmit}>
+    //             <label>Enter Title:</label>
+    //             <input
+    //                 type='text'
+    //                 value={title}
+    //                 onChange={(e) => setTitle(e.target.value)}
+    //             />
+    //             {clientErrors.title && <span>{clientErrors.title}</span>}
+    //             <br />
+
+    //             <label>Enter Price:</label>
+    //             <input
+    //                 type='number'
+    //                 value={price}
+    //                 onChange={(e) => setPrice(e.target.value)}
+    //             />
+    //             {clientErrors.price && <span>{clientErrors.price}</span>}
+    //             <br />
+
+    //             <label>Enter Quantity:</label>
+    //             <input
+    //                 type='number'
+    //                 value={quantity}
+    //                 onChange={(e) => setQuantity(e.target.value)}
+    //             />
+    //             {clientErrors.quantity && <span>{clientErrors.quantity}</span>}
+    //             <br />
+
+    //             <label>Enter Description:</label>
+    //             <textarea
+    //                 value={description}
+    //                 onChange={(e) => setDescription(e.target.value)}
+    //             />
+    //             {clientErrors.description && <span>{clientErrors.description}</span>}
+    //             <br />
+
+    //             <label>Select Category:</label>
+    //             <select value={category} onChange={(e) => setCategory(e.target.value)}>
+    //                 <option value=''>Select</option>
+    //                 {categories?.map((ele) => (
+    //                     <option value={ele._id} key={ele._id}>{ele.name}</option>
+    //                 ))}
+    //             </select>
+    //             <br />
+
+    //             <label>Is Approved:</label>
+    //             <select value={isApproved} onChange={(e) => setIsApproved(e.target.value === 'true')}>
+    //                 <option value=''>Select</option>
+    //                 <option value='false'>False</option>
+    //                 <option value='true'>True</option>
+    //             </select>
+    //             <br />
+
+    //             <input type='submit' value='Create Product' />
+    //         </form>
+    //     </div>
+    // )
+
+
     return (
-        <div>
-            <h1>Create Product</h1>
+        <div className="product-form-container">
+            <h1 className="form-title">Create Product</h1>
 
             {serverErrors?.errors && (
-                <>
-                    <p>These errors are prohibited from creating the category:</p>
+                <div className="server-errors">
+                    <p>These errors are prohibited from creating the product:</p>
                     <ul>
                         {serverErrors.errors.map((ele, i) => (
                             <li key={i}>{ele.msg}</li>
                         ))}
                     </ul>
-                </>
+                </div>
             )}
-            <form onSubmit={handleSubmit}>
+
+            <form onSubmit={handleSubmit} className="product-form">
                 <label>Enter Title:</label>
                 <input
                     type='text'
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
+                    className="form-input"
                 />
-                {clientErrors.title && <span>{clientErrors.title}</span>}
-                <br />
+                {clientErrors.title && <span className="error-text">{clientErrors.title}</span>}
 
                 <label>Enter Price:</label>
                 <input
                     type='number'
                     value={price}
                     onChange={(e) => setPrice(e.target.value)}
+                    className="form-input"
                 />
-                {clientErrors.price && <span>{clientErrors.price}</span>}
-                <br />
+                {clientErrors.price && <span className="error-text">{clientErrors.price}</span>}
 
                 <label>Enter Quantity:</label>
                 <input
                     type='number'
                     value={quantity}
                     onChange={(e) => setQuantity(e.target.value)}
+                    className="form-input"
                 />
-                {clientErrors.quantity && <span>{clientErrors.quantity}</span>}
-                <br />
+                {clientErrors.quantity && <span className="error-text">{clientErrors.quantity}</span>}
 
-                <label>Enter Description:</label>
+                <label>Enter product Description:</label>
                 <textarea
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
+                    className="form-textarea"
                 />
-                {clientErrors.description && <span>{clientErrors.description}</span>}
-                <br />
+                {clientErrors.description && <span className="error-text">{clientErrors.description}</span>}
 
                 <label>Select Category:</label>
-                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                    className="form-input"
+                >
                     <option value=''>Select</option>
                     {categories?.map((ele) => (
                         <option value={ele._id} key={ele._id}>{ele.name}</option>
                     ))}
                 </select>
-                <br />
 
                 <label>Is Approved:</label>
-                <select value={isApproved} onChange={(e) => setIsApproved(e.target.value === 'true')}>
+                <select
+                    value={isApproved}
+                    onChange={(e) => setIsApproved(e.target.value === 'true')}
+                    className="form-input"
+                >
                     <option value=''>Select</option>
                     <option value='false'>False</option>
                     <option value='true'>True</option>
                 </select>
-                <br />
 
-                <input type='submit' value='Create Product' />
+                <input
+                    type='submit'
+                    value='Create Product'
+                    className="submit-button"
+                />
             </form>
         </div>
     )
+
 }
 
 export default CreateProduct
